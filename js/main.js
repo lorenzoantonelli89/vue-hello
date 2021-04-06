ab071ee15c8cc2847ef961e3e7bd90603ec33145
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-04-06 14:26:29 +0200

    update aggiunto btn per change img creato un carousel in pratica

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@ function initVue() {
         el: '#app',
         data: {
             'text': 'Hello World!!',
-            'image': 'boat.jpg'
+            'image': 'img/boat.jpg'
         },
+        methods: {
+            changeImg: function(){
+                if (this.image == 'img/boat.jpg') {
+                    this.image = 'img/boat2.JPG'
+                }else if (this.image == 'img/boat2.JPG'){
+                    this.image = 'img/boat3.JPG'
+                }else{
+                    this.image = 'img/boat.jpg'
+                }
+            }
+        }
         
     });
 }
